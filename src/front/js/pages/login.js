f6b3887a4368c2456c5d0cbcd0eb93d3a9b05ef3
Katheren Salom <katherensalom@gmail.com>
2022-08-14 21:37:07 +0000

    modified:   src/front/js/layout.js new file:   src/front/js/pages/login.js

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+	const {email, setEmail} = useState("");
+	const {password, setPassword} = useState("");
+
+	const handleClick = () => {
+		const opts={
+			method: "POST",
+			body: JSON.stringify([{
+				"email": email,
+				"password": password,
+			}]),
+			Headers: {
+				"Content-Type": "application/json"
+			}
+		}
+
+		fetch('https://3001-mevctrader-proyectopsiq-it34iqpat6j.ws-us60.gitpod.io/api/token/',opts)
+			.then(resp => {
+				if(resp.status===200) return resp.json();
+				else alert("otro error en el servidor");
+			})
+			.then()
+			.catch(error => {
+				console.log("error en el servidor: ", error)
+			})
+	}
+	return (
+		<div className="text-center">
+			<h1>Login</h1>
+			<div>
+				<input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value) }/>
+				<input type="password"  placeholder="password" value={password} onChange={(e) => setPassword(e.target.value) }/>
+				<button onClick={handleClick}>Entrar</button>
+			</div>
+		</div>
+	);
+};
