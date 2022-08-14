66070bf942888d9549ec6e8398db901d9eb0d894
Katheren Salom <katherensalom@gmail.com>
2022-08-14 23:23:36 +0000

    modified:   src/front/js/pages/login.js

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -13,22 +13,22 @@ export const Login = () => {
 			method: "POST",
 			body: JSON.stringify({
 				"email": email,
-				"password": password
+				"password": password,
 			}),
 			Headers: {
 				"Content-Type": "application/json"
 			}
-		}
+		};
 
-		fetch('https://3001-mevctrader-proyectopsiq-9tai7vq698y.ws-us60.gitpod.io/api/token/',opts)
+		fetch('https://3001-mevctrader-proyectopsiq-9tai7vq698y.ws-us60.gitpod.io/api/token', opts)
 			.then(resp => {
-				if(resp.status===200) return resp.json();
+				if(resp.status==200) return resp.json();
 				else alert("otro error en el servidor");
 			})
 			.then()
 			.catch(error => {
 				console.log("error en el servidor: ", error)
-			})
+			});
 	}
 	return (
 		<div className="text-center">
