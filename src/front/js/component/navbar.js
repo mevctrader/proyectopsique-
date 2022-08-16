39b8ea4eda976442e9eaa2feeb34d06137ab5db8
mevctrader <mevctrader@gmail.com>
2022-08-16 21:01:16 +0000

    matio navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,99 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import psiqueImageUrl from "../../img/Psique2.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    //    <nav className="navbar navbar-light bg-light">
+    //      <div className="container">
+    //        <Link to="/">
+    //          <img className="navbar-brand mb-0 h1" src={psiqueImageUrl} />
+    //       </Link>
+    //        <div className="ml-auto">
+    //          <Link to="/demo">
+    //            <button className="btn btn-primary">
+    //              Check the Context in action
+    //            </button>
+    //          </Link>
+    //        </div>
+    //      </div>
+    //    </nav>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src={psiqueImageUrl} alt="" width="100" height="50" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,45 +1,55 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import psiqueImageUrl from "../../img/Psique1.png";
 import "../../styles/home.css";
 
 export const Login = () => {
-	const { store, actions } = useContext(Context);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const handleClick = () => {
-		const opts={
-			method: "POST",
-			body: JSON.stringify({
-				"email": email,
-				"password": password,
-			}),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		};
+  const handleClick = () => {
+    const opts = {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-		fetch(process.env.BACKEND_URL+'/api/token', opts)
-			.then(resp => {
-				if(resp.status==200) return resp.json();
-				else alert("otro error en el servidor");
-			})
-			.then(data =>{
-				console.log(data);
-			})
-			.catch(error => {
-				console.log("error en el servidor: ", error)
-			});
-	}
-	return (
-		<div className="text-center">
-			<h1>Login</h1>
-			<div>
-				<input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-				<input type="password"  placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-				<button onClick={handleClick}>Entrar</button>
-			</div>
-		</div>
-	);
+    fetch(process.env.BACKEND_URL + "/api/token", opts)
+      .then((resp) => {
+        if (resp.status == 200) return resp.json();
+        else alert("otro error en el servidor");
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log("error en el servidor: ", error);
+      });
+  };
+  return (
+    <div className="text-center">
+      <h1>Login</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleClick}>Entrar</button>
+      </div>
+    </div>
+  );
 };
