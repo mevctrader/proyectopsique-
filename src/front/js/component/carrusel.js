eb6d5bd91844e8d288887155509f63f931113294
mevctrader <mevctrader@gmail.com>
2022-08-16 23:07:57 +0000

    navbar login

--- src/front/js/component/carrusel.js
+++ src/front/js/component/carrusel.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import psiqueImageUrl from "../../img/Psique2.png";
 
 export const Carrusel = () => (
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-    <div class="carousel-indicators">
+  <div
+    id="carouselExampleCaptions"
+    className="carousel slide"
+    data-bs-ride="false"
+  >
+    <div className="carousel-indicators">
       <button
         type="button"
         data-bs-target="#carouselExampleCaptions"
         data-bs-slide-to="0"
-        class="active"
+        className="active"
         aria-current="true"
         aria-label="Slide 1"
       ></button>
@@ -25,46 +29,46 @@ export const Carrusel = () => (
         aria-label="Slide 3"
       ></button>
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src={psiqueImageUrl} class="d-block w-100" alt="..." />
-        <div class="carousel-caption d-none d-md-block">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src={psiqueImageUrl} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block">
           <h5>First slide label</h5>
           <p>Some representative placeholder content for the first slide.</p>
         </div>
       </div>
-      <div class="carousel-item">
-        <img src={psiqueImageUrl} class="d-block w-100" alt="..." />
-        <div class="carousel-caption d-none d-md-block">
+      <div className="carousel-item">
+        <img src={psiqueImageUrl} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block">
           <h5>Second slide label</h5>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
-      <div class="carousel-item">
-        <img src={psiqueImageUrl} class="d-block w-100" alt="..." />
-        <div class="carousel-caption d-none d-md-block">
+      <div className="carousel-item">
+        <img src={psiqueImageUrl} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block">
           <h5>Third slide label</h5>
           <p>Some representative placeholder content for the third slide.</p>
         </div>
       </div>
     </div>
     <button
-      class="carousel-control-prev"
+      className="carousel-control-prev"
       type="button"
       data-bs-target="#carouselExampleCaptions"
       data-bs-slide="prev"
     >
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
     </button>
     <button
-      class="carousel-control-next"
+      className="carousel-control-next"
       type="button"
       data-bs-target="#carouselExampleCaptions"
       data-bs-slide="next"
     >
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
     </button>
   </div>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,27 +7,13 @@ export const Navbar = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    //    <nav className="navbar navbar-light bg-light">
-    //      <div className="container">
-    //        <Link to="/">
-    //          <img className="navbar-brand mb-0 h1" src={psiqueImageUrl} />
-    //       </Link>
-    //        <div className="ml-auto">
-    //          <Link to="/demo">
-    //            <button className="btn btn-primary">
-    //              Check the Context in action
-    //            </button>
-    //          </Link>
-    //        </div>
-    //      </div>
-    //    </nav>
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid row">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
           <img src={psiqueImageUrl} alt="" width="100" height="50" />
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -35,66 +21,72 @@ export const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+        <div className="container-fluid">
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Buscar
+            </button>
+          </form>
+        </div>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Blog
               </a>
             </li>
-            <li class="nav-item dropdown">
+            <li className="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
+                className="nav-link dropdown-toggle"
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Dropdown
+                Registro
               </a>
-              <ul class="dropdown-menu">
+              <ul className="dropdown-menu">
+                {!store.token ? (
+                  <Link to="/login">
+                    <button className="btn btn-primary">Log in</button>
+                  </Link>
+                ) : (
+                  <button
+                    onClick={() => actions.logout()}
+                    className="btn btn-primary"
+                  >
+                    Log out
+                  </button>
+                )}
                 <li>
-                  <a class="dropdown-item" href="#">
-                    Action
+                  <a className="dropdown-item" href="#">
+                    SignUp
                   </a>
                 </li>
                 <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
+                  <hr className="dropdown-divider" />
                 </li>
               </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Test
+              </a>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
