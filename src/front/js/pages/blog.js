b179732efdc31754fb2a380ab508a51cecc88d20
vyanse <valentinayulloa@gmail.com>
2022-08-18 19:43:36 +0000

    blog parte 1

--- src/front/js/pages/blog.js
+++ src/front/js/pages/blog.js
@@ -1,6 +1,85 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>AQUI ES DONDE VAMOS A CODEAR</div>;
+  return (
+  <div 
+      className="display: flex flex-direction: row">  
+       <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+       <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+       <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+       <div className="card-body">
+       <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+       <a href="#" className="btn btn-primary">Leer mas</a>
+       </div>
+       </div>
+
+       <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+
+        <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+
+
+        <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+
+
+        <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+
+
+        <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+
+        <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+
+        <div className="card mb-5 ms-3" style={{ width: "18rem" }}>
+        <div> <h5 className="card-title">La depresion y sus sintomas</h5> </div>
+         <img src="https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg" className="card-img-top w-100" alt="..."/>
+        <div className="card-body">
+        <p className="card-text"> Es evidente que la depresion es una patologia muy presente hoy en dia, miles de personas la padecen. En España se estima que entre el 5 y el 10% de la poblacion podria tener depresion, es decir, de 2 a 4 millones de personas, no obstante esta cifra podria ascender ... </p>
+        <a href="#" className="btn btn-primary">Leer mas</a>
+        </div>
+        </div>
+  </div>
+  );
+  
 };
 export default Blog;
