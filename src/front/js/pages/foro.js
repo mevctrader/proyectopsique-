b6db8aa83aac5f123ff9bc2214ae2f8a032e1637
vyanse <valentinayulloa@gmail.com>
2022-08-23 19:31:15 +0000

    foro avance

--- src/front/js/pages/foro.js
+++ src/front/js/pages/foro.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
 
 const Foro = () => {
   return <div>
-  <div className="list-group justify-content w-25 p-5 mw-100">
+  <div className="list-group justify-content w-25 p-5 mw-100 display-flex-row">
   <a class="list-group-item list-group-item-action disabled">Topicos</a>
 <a href="#" className="list-group-item list-group-item-action">#General</a>
 <a href="#" className="list-group-item list-group-item-action">#Ansiedad</a>
@@ -10,7 +11,26 @@ const Foro = () => {
 <a href="#" className="list-group-item list-group-item-action">#Depresión</a>
 <a href="#" className="list-group-item list-group-item-action">#AYUDAME</a>
 </div>
+<div>
+<Form>
+<Form.Group className=" w-50 p-5 mb-2  " controlId="exampleForm.ControlInput1">
+        <Form.Label>Nueva Publicacion</Form.Label>
+        <Form.Control type="email" placeholder="texto" />
 
+       <div> <button type="button" className="btn btn-primary mt-3">Enviar</button>
+
+       </div>
+      </Form.Group>
+      <Form.Group className="justify-content w-50 p-5 mb-2" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Comentarios</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+    </div>
 </div>;
 };
+
+
+
+
 export default Foro;
