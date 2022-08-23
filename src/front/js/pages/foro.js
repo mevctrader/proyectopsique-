ffb7ee50786734f005509ac020a12fb09ab1cf40
vyanse <valentinayulloa@gmail.com>
2022-08-23 21:43:18 +0000

    mejora del foro

--- src/front/js/pages/foro.js
+++ src/front/js/pages/foro.js
@@ -1,29 +1,51 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 const Foro = () => {
-  return <div>
-  <div className="list-group justify-content w-25 p-5 mw-100 display-flex-row">
-  <a class="list-group-item list-group-item-action disabled">Topicos</a>
-<a href="#" className="list-group-item list-group-item-action">#General</a>
-<a href="#" className="list-group-item list-group-item-action">#Ansiedad</a>
-<a href="#" className="list-group-item list-group-item-action">#Estres</a>
-<a href="#" className="list-group-item list-group-item-action">#Depresión</a>
-<a href="#" className="list-group-item list-group-item-action">#AYUDAME</a>
-</div>
-<div>
-<Form>
-      <Form.Group className="justify-content w-50 p-5 mb-2" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Nueva Publicación</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-        <div> <button type="button" className="btn btn-primary mt-3">Enviar</button> </div>
-      </Form.Group>
-    </Form>
+  return (
+    <div className="d-flex flex-row">
+      <div className="d-flex flex-row col-12">
+        <div className="col-6 list-group justify-content w-25 p-5">
+          <a className="list-group-item list-group-item-action disabled">Topicos</a>
+          <a href="#" className="list-group-item list-group-item-action">
+            #General
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            #Ansiedad
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            #Estres
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            #Depresión
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            #AYUDAME
+          </a>
+        </div>
+        <div className="col-6">
+          <Form>
+            <Form.Group
+              className="justify-content mt-5"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Nueva Publicación</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+              <div>
+                {" "}
+                <button
+                  type="button"
+                  className="btn btn-primary mt-3"
+                >
+                  Enviar
+                </button>{" "}
+              </div>
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
     </div>
-</div>;
+  );
 };
-
-
-
 
 export default Foro;
