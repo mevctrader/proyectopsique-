38cd7ca6eee72f6497ed5440a75de5b38fedf792
mevctrader <mevctrader@gmail.com>
2022-08-24 19:48:23 +0000

    ajustes enlacees footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import psiqueImageUrl from "../../img/Psiento1.png";
-import instagramLogo from "../../img/instagram-svgrepo-com-01.png"
-import twitterLogo from "../../img/twitter-svgrepo-com-01.png"
-import mailLogo from "../../img/mail_FILL1_wght400_GRAD0_opsz48-01.png"
+import instagramLogo from "../../img/instagram-svgrepo-com-01.png";
+import twitterLogo from "../../img/twitter-svgrepo-com-01.png";
+import mailLogo from "../../img/mail_FILL1_wght400_GRAD0_opsz48-01.png";
 
 export const Footer = () => (
   <div
@@ -28,17 +28,20 @@ export const Footer = () => (
 
       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
         <li className="ms-3">
-          <a className="text-muted" href="#">
+          <a
+            className="text-muted"
+            href="https://www.instagram.com/psientowell/"
+          >
             <img className="bi" width="24" height="24" src={instagramLogo} />
           </a>
         </li>
         <li className="ms-3">
-          <a className="text-muted" href="#">
+          <a className="text-muted" href="https://twitter.com/PsientoWell">
             <img className="bi" width="24" height="24" src={twitterLogo} />
           </a>
         </li>
         <li className="ms-3">
-          <a className="text-muted" href="#">
+          <a className="text-muted" href="mailto:psientowell@gmail.com">
             <img className="bi" width="24" height="24" src={mailLogo} />
           </a>
         </li>
--- src/front/js/pages/videos.js
+++ src/front/js/pages/videos.js
@@ -4,89 +4,95 @@ const Videos = () => {
   return (
     <div id="registro-div">
       <div
-      id="carouselExampleIndicators"
-      className="carousel slide"
-      data-bs-ride="true"
-    >
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <iframe
-            className="ratio ratio-16x9"
-            width="1200"
-            height="600"
-            src="https://www.youtube.com/embed/M_no3cxZJws"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <div className="carousel-item">
-          <iframe
-            className="ratio ratio-16x9"
-            width="1200"
-            height="600"
-            src="https://www.youtube.com/embed/u1DcrhgLO44"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <div className="carousel-item">
-          <iframe
-            className="ratio ratio-16x9"
-            width="1200"
-            height="600"
-            src="https://www.youtube.com/embed/sFfxExoU2Kw"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="prev"
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-bs-ride="true"
       >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <iframe
+              className="ratio ratio-16x9"
+              width="1200"
+              height="600"
+              src="https://www.youtube.com/embed/M_no3cxZJws"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="carousel-item">
+            <iframe
+              className="ratio ratio-16x9"
+              width="1200"
+              height="600"
+              src="https://www.youtube.com/embed/u1DcrhgLO44"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="carousel-item">
+            <iframe
+              className="ratio ratio-16x9"
+              width="1200"
+              height="600"
+              src="https://www.youtube.com/embed/sFfxExoU2Kw"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 };
