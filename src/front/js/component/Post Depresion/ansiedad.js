e2ba6859ba0261bf425c0350870b48ab28600c5b
vyanse <valentinayulloa@gmail.com>
2022-08-27 18:23:57 +0000

    depresion y ansiedad

--- src/front/js/component/Post Depresion/ansiedad.js
+++ src/front/js/component/Post Depresion/ansiedad.js
@@ -8,37 +8,33 @@ import image4 from "../../../img/Psiento1.png";
 const postinfo = [
   {
     id: 1,
-    title: "La ansiedad y sus sintomas",
-    image:
-      "https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, molestias quae. Atque ipsum, et nobis aliquam voluptas eos sapiente sit natus obcaecati, ipsam est explicabo nihil repellendus placeat. Rem, quasi. Iste harum veniam magnam distinctio amet rerum dignissimos facilis inventore impedit deserunt reiciendis iusto voluptas autem voluptatum, necessitatibus ea cupiditate voluptates cum quidem quibusdam suscipit nemo tempore? Quia, pariatur esse? Iure magnam tempora laboriosam distinctio ullam, possimus culpa? Fuga molestiae dicta ipsum enim amet itaque iste qui ea, dignissimos, culpa aliquid minus illum, vel iure corporis aperiam at ullam porro.",
+    title: "ANSIEDAD",
+    image:"https://terapi-app.com/blog/wp-content/uploads/2022/02/ansiedad-9-400x300.png",
+    text: "La ansiedad es un sentimiento de miedo, temor e inquietud. Puede hacer que sude, se sienta inquieto y tenso, y tener palpitaciones. Puede ser una reacción normal al estrés. Por ejemplo, puede sentirse ansioso cuando se enfrenta a un problema difícil en el trabajo, antes de tomar un examen o antes de tomar una decisión importante. Si bien la ansiedad puede ayudar a enfrentar una situación, además de darle un impulso de energía o ayudarle a concentrarse, para las personas con trastornos de ansiedad el miedo no es temporal y puede ser abrumadora.",
     modal: "modal9",
     modaltargeter: "#modal9",
   },
   {
     id: 2,
-    title: "La depresión y sus sintomas",
-    image:
-      "https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, molestias quae. Atque ipsum, et nobis aliquam voluptas eos sapiente sit natus obcaecati, ipsam est explicabo nihil repellendus placeat. Rem, quasi. Iste harum veniam magnam distinctio amet rerum dignissimos facilis inventore impedit deserunt reiciendis iusto voluptas autem voluptatum, necessitatibus ea cupiditate voluptates cum quidem quibusdam suscipit nemo tempore? Quia, pariatur esse? Iure magnam tempora laboriosam distinctio ullam, possimus culpa? Fuga molestiae dicta ipsum enim amet itaque iste qui ea, dignissimos, culpa aliquid minus illum, vel iure corporis aperiam at ullam porro.",
+    title: "SINTOMAS",
+    image:"https://piel-l.org/blog/wp-content/uploads//2016/05/angustia-cuadro.jpg",
+    text: "Los diferentes tipos de trastornos de ansiedad pueden tener síntomas diferentes. Pero todos muestran una combinación de: Pensamientos o creencias ansiosos difíciles de controlar: Le hacen sentir inquieto y tenso e interfieren con su vida diaria. No desaparecen y pueden empeorar con el tiempo. Síntomas físicos, como latidos cardíacos fuertes o rápidos, dolores y molestias inexplicables, mareos y falta de aire. Cambios en el comportamiento, como evitar las actividades cotidianas que solía hacer. NOTA: El uso de cafeína, otras sustancias y ciertos medicamentos pueden empeorar sus síntomas.",
     modal: "modal10",
     modaltargeter: "#modal10",
   },
   {
     id: 3,
-    title: "La depresión y sus sintomas",
-    image:
-      "https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, molestias quae. Atque ipsum, et nobis aliquam voluptas eos sapiente sit natus obcaecati, ipsam est explicabo nihil repellendus placeat. Rem, quasi. Iste harum veniam magnam distinctio amet rerum dignissimos facilis inventore impedit deserunt reiciendis iusto voluptas autem voluptatum, necessitatibus ea cupiditate voluptates cum quidem quibusdam suscipit nemo tempore? Quia, pariatur esse? Iure magnam tempora laboriosam distinctio ullam, possimus culpa? Fuga molestiae dicta ipsum enim amet itaque iste qui ea, dignissimos, culpa aliquid minus illum, vel iure corporis aperiam at ullam porro.",
+    title: "TIPOS DE ANSIEDAD",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStElDUbOAFuIiTdVnYqfQfDjsDmZLQlJZzww&usqp=CAU",
+    text: "Existen varios tipos de trastornos de ansiedad, incluyendo: Trastorno de ansiedad generalizada: Las personas con este trastorno se preocupan por problemas comunes como la salud, el dinero, el trabajo y la familia. Pero sus preocupaciones son excesivas y las tienen casi todos los días durante al menos 6 meses. TRASTORNO DE PANICO: Las personas con trastorno de pánico sufren de ataques de pánico. Estos son repentinos y repetidos momentos de miedo intenso sin haber un peligro aparente. Los ataques se producen rápidamente y pueden durar varios minutos o más. FOBIAS: Las personas con fobias tienen un miedo intenso a algo que representa poco o ningún peligro real. Su miedo puede ser por arañas, volar, ir a lugares concurridos o estar en situaciones sociales (conocida como ansiedad social)",
     modal: "modal11",
     modaltargeter: "#modal11",
   },
   {
     id: 4,
-    title: "La depresión y sus sintomas",
-    image:
-      "https://www.saludterapia.com/images/saludterapia/glosario/psicologia/psicologia.jpg",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, molestias quae. Atque ipsum, et nobis aliquam voluptas eos sapiente sit natus obcaecati, ipsam est explicabo nihil repellendus placeat. Rem, quasi. Iste harum veniam magnam distinctio amet rerum dignissimos facilis inventore impedit deserunt reiciendis iusto voluptas autem voluptatum, necessitatibus ea cupiditate voluptates cum quidem quibusdam suscipit nemo tempore? Quia, pariatur esse? Iure magnam tempora laboriosam distinctio ullam, possimus culpa? Fuga molestiae dicta ipsum enim amet itaque iste qui ea, dignissimos, culpa aliquid minus illum, vel iure corporis aperiam at ullam porro.",
+    title: "TRATAMIENTOS",
+    image:"https://terapi-app.com/blog/wp-content/uploads/2021/04/Copia-de-Sin-ti%CC%81tulo-400x300.jpg",
+    text: "Los principales tratamientos para los trastornos de ansiedad son psicoterapia (terapia de conversación), medicamentos o ambos: La terapia cognitiva conductual es un tipo de psicoterapia que a menudo se usa para tratar los trastornos de ansiedad. Enseña diferentes formas de pensar y comportarse. Puede ayudarle a cambiar cómo reacciona ante las cosas que le causan miedo y ansiedad. Puede incluir terapia de exposición, la que se enfoca en confrontar sus miedos para que pueda hacer las cosas que ha estado evitando. Los medicamentos para tratar los trastornos de ansiedad incluyen medicamentos contra la ansiedad y ciertos antidepresivos. Algunos tipos de medicamentos pueden funcionar mejor para tipos específicos de trastornos de ansiedad. Debe comunicarse bien con su profesional de la salud para identificar qué medicamento es mejor para usted. Es posible que deba probar más de un medicamento antes de encontrar el correcto",
     modal: "modal12",
     modaltargeter: "#modal12",
   },
