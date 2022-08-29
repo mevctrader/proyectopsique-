b1def9416dbe4597854b231b32d4a351d27fff2b
mevctrader <mevctrader@gmail.com>
2022-08-29 15:27:10 +0000

    foro primer approach

--- src/front/js/pages/foro.js
+++ src/front/js/pages/foro.js
@@ -3,10 +3,81 @@ import Form from "react-bootstrap/Form";
 
 const Foro = () => {
   return (
-    <div id="foro-div" className="d-flex flex-row">
+    <div id="foro-div" className="container-fluid">
+      <div id="boton-post" className="container-fluid justify-content w-30 p-5">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          data-bs-whatever="@mdo"
+        >
+          NUEVO POST
+        </button>
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  NUEVO POST
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label for="recipient-name" className="col-form-label">
+                      TÓPICO:
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="recipient-name"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label for="message-text" className="col-form-label">
+                      MENSAJE:
+                    </label>
+                    <textarea
+                      className="form-control"
+                      id="message-text"
+                    ></textarea>
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Cerrar
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Enviar Mensaje
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="d-flex flex-row col-12">
         <div className="col-6 list-group justify-content w-25 p-5">
-          <a className="list-group-item list-group-item-action disabled">Topicos</a>
+          <a className="list-group-item list-group-item-action disabled">
+            <strong>TÓPICOS</strong>
+          </a>
           <a href="#" className="list-group-item list-group-item-action">
             #General
           </a>
@@ -23,25 +94,69 @@ const Foro = () => {
             #AYUDAME
           </a>
         </div>
-        <div className="col-6">
-          <Form>
-            <Form.Group
-              className="justify-content mt-5"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Nueva Publicación</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-              <div>
-                {" "}
-                <button
-                  type="button"
-                  className="btn btn-primary mt-3"
-                >
-                  Enviar
-                </button>{" "}
-              </div>
-            </Form.Group>
-          </Form>
+        <div className="col-6 list-group justify-content w-30 p-5 ">
+          <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">PRIMERA PUBLICACIÓN</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1 cols-120">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
+            </p>
+            <small className="text-muted justify-content-between p-5">
+              #Ansiedad
+            </small>
+            <button type="button" className="btn btn-primary btn-sm">
+              COMENTAR
+            </button>
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">SEGUNDA PUBLICACIÓN</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1 cols-120">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
+            </p>
+            <small className="text-muted justify-content-between p-5">
+              #Depresion
+            </small>
+            <button type="button" className="btn btn-primary btn-sm">
+              COMENTAR
+            </button>
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">TERCERA PUBLICACIÓN</h5>
+              <small className="text-muted ">3 days ago</small>
+            </div>
+            <p className="mb-1 cols-120">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
+            </p>
+            <small className="text-muted justify-content-between p-5">
+              #Estres
+            </small>
+            <button type="button" className="btn btn-primary btn-sm">
+              COMENTAR
+            </button>
+          </a>
+          {/* <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1">Some placeholder content in a paragraph.</p>
+            <small className="text-muted">And some muted small print.</small>
+          </a> */}
         </div>
       </div>
     </div>
