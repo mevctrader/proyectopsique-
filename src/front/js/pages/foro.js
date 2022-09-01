13a7b73d23cf7dfb87b195d061a6bee765a7563b
Lgxa19 <larm.gd@gmail.com>
2022-09-01 04:43:04 +0000

    Estructura foro2

--- src/front/js/pages/foro.js
+++ src/front/js/pages/foro.js
@@ -37,7 +37,7 @@ const Foro = () => {
         </div>
       </div>
       <div className="col-sm-10 row p-0 m-0">
-          <div className="col-md-3">
+          <div className="col-md-3 py-2">
             <Card border="success">
               <Card.Header>Tópicos: Ansiedd</Card.Header>
               <Card.Body>
@@ -50,7 +50,7 @@ const Foro = () => {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-3 py-2">
             <Card border="success">
               <Card.Header>Tópicos: Ansiedd</Card.Header>
               <Card.Body>
@@ -63,7 +63,7 @@ const Foro = () => {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-3 py-2">
             <Card border="success">
               <Card.Header>Tópicos: Ansiedd</Card.Header>
               <Card.Body>
@@ -76,7 +76,59 @@ const Foro = () => {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-3 py-2">
+            <Card border="success">
+              <Card.Header>Tópicos: Ansiedd</Card.Header>
+              <Card.Body>
+                <Card.Title>Titulo: Tengo ansiedad</Card.Title>
+                <Card.Text>
+                  Descripción: Ayuda tengo demasiada ansiedad ojala salgamos
+                  bien help
+                </Card.Text>
+                <Card.Link href="#">Leer Más</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-3 py-2">
+            <Card border="success">
+              <Card.Header>Tópicos: Ansiedd</Card.Header>
+              <Card.Body>
+                <Card.Title>Titulo: Tengo ansiedad</Card.Title>
+                <Card.Text>
+                  Descripción: Ayuda tengo demasiada ansiedad ojala salgamos
+                  bien help
+                </Card.Text>
+                <Card.Link href="#">Leer Más</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-3 py-2">
+            <Card border="success">
+              <Card.Header>Tópicos: Ansiedd</Card.Header>
+              <Card.Body>
+                <Card.Title>Titulo: Tengo ansiedad</Card.Title>
+                <Card.Text>
+                  Descripción: Ayuda tengo demasiada ansiedad ojala salgamos
+                  bien help
+                </Card.Text>
+                <Card.Link href="#">Leer Más</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-3 py-2">
+            <Card border="success">
+              <Card.Header>Tópicos: Ansiedd</Card.Header>
+              <Card.Body>
+                <Card.Title>Titulo: Tengo ansiedad</Card.Title>
+                <Card.Text>
+                  Descripción: Ayuda tengo demasiada ansiedad ojala salgamos
+                  bien help
+                </Card.Text>
+                <Card.Link href="#">Leer Más</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-3 py-2">
             <Card border="success">
               <Card.Header>Tópicos: Ansiedd</Card.Header>
               <Card.Body>
